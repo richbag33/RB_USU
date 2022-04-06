2882b59743f899c1cd46bda9af1caa51e2f8a34e
richbag33 <102702131+richbag33@users.noreply.github.com>
2022-04-06 12:48:32 -0600

    Update scapydns.py

--- scapydns.py
+++ scapydns.py
@@ -1,1 +1,10 @@
-sudo scapy
+>>> dnsRequest = IP(dst='8.8.8.8')/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname='www.facebook.com'))  
+
+>>> dnsResponse = sr1(dnsRequest)   
+
+# Begin emission:
+# Finished sending 1 packets.
+# .*
+# Received 2 packets, got 1 answers, remaining 0 packets
+>>> print(dnsResponse[DNS].summary())                                                          
+# DNS Ans "b'star-mini.c10r.facebook.com.'" 
